7a1dff92eed8a7e132f7a1ef32f930a54b50e97e
Sarvan Thakor <97446457+sarvanthakor01@users.noreply.github.com>
2025-02-16 17:47:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,41 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(species) {
+    super(species); // Call the parent class constructor
+  }
+
+  // Dog-specific method
+  bark() {
+    console.log("woof");
+  }
+}
+
+class Cat extends Animal {
+  constructor(species) {
+    super(species); // Call the parent class constructor
+  }
+
+  // Cat-specific method
+  purr() {
+    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
